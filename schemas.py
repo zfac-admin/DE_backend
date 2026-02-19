64c80a1e5b74236a0c5a49ea5fedee03e0538881
zfac-dev <dgla3155@gmail.com>
2026-02-20 06:24:15 +0900

    schema update2

--- schemas.py
+++ schemas.py
@@ -67,8 +67,8 @@ class ProductionCreate(BaseModel):
     produced_quantity: int
     production_efficiency: int
     process: str
-    operating_time: time
-    non_operating_time: time
+    operating_time: Optional[time] = None
+    non_operating_time: Optional[time] = None
     shift: str
     line_efficiency: int
     specification: str
